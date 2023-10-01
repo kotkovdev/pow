f7a34e74fc3f272b6c53837c1c06dadf440dfa56
kotkovdev <kotkovdev@gmail.com>
2023-10-01 15:10:25 +0300

    refactor client and update readme

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,52 +1,31 @@
 package main
 
 import (
-	"bufio"
 	"encoding/base64"
 	"flag"
+	"fmt"
 	"log/slog"
-	"net"
-	"strings"
 
 	"github.com/kotkovdev/pow/pkg/challenger"
 
-	"github.com/kotkovdev/pow/internal/util"
+	"github.com/kotkovdev/pow/internal/client"
 )
 
 func main() {
 	var address string
 	flag.StringVar(&address, "address", ":8080", "sets server connection address")
 	flag.Parse()
-	conn, err := net.Dial("tcp", address)
+
+	cli, err := client.New(address)
 	if err != nil {
 		slog.Error("could not connect to server", "error", err, "address", address)
 		return
 	}
-	defer conn.Close()
+	defer cli.Close()
 
-	util.Send(nil, conn)
-	slog.Info("sent connection request")
-
-	resp, err := bufio.NewReader(conn).ReadString(util.MessageDelimeter)
+	source, target, err := cli.RequestPuzzle()
 	if err != nil {
-		slog.Error(err.Error())
-		return
-	}
-
-	// clear delimeter
-	resp = resp[:len(resp)-1]
-	slog.Info("got response", "response", resp)
-
-	parts := strings.Split(resp, string(util.Separator))
-	sourceStr, targetStr := parts[0], parts[1]
-	source, err := base64.StdEncoding.DecodeString(sourceStr)
-	if err != nil {
-		slog.Error("could not parse source hash", "error", err)
-		return
-	}
-	target, err := base64.StdEncoding.DecodeString(targetStr)
-	if err != nil {
-		slog.Error("could not parse target hash", "error", err)
+		slog.Error("could not get puzzle", "error", err)
 		return
 	}
 
@@ -54,17 +33,13 @@ func main() {
 	answer := chal.SolveRecursive(source, target)
 
 	answerStr := base64.StdEncoding.EncodeToString(answer)
-	if err := util.Send([]byte(answerStr), conn); err != nil {
-		slog.Error("could not send request", "error", err)
-		return
-	}
-	slog.Info("found solution", "answer", answerStr, "source", sourceStr, "target", targetStr)
-	slog.Info("sent answer request")
+	slog.Info("found solution", "answer", answerStr)
 
-	resp, err = bufio.NewReader(conn).ReadString(util.MessageDelimeter)
+	quote, err := cli.ReqeustQuote(answerStr)
 	if err != nil {
-		slog.Error("could not parse response", "error", err)
+		slog.Error("could not request quote", "error", err)
 		return
 	}
-	slog.Info("got response", "response", resp)
+
+	fmt.Println(string(quote))
 }
---
+++ internal/client/client.go
@@ -0,0 +1,82 @@
+// package client provides client for connection to server.
+package client
+
+import (
+	"encoding/base64"
+	"log/slog"
+	"net"
+	"strings"
+
+	"github.com/kotkovdev/pow/internal/util"
+)
+
+const (
+	clientProtocol = "tcp"
+)
+
+type Client struct {
+	conn net.Conn
+}
+
+func New(address string) (Client, error) {
+	conn, err := net.Dial(clientProtocol, address)
+	if err != nil {
+		return Client{}, err
+	}
+
+	return Client{
+		conn: conn,
+	}, nil
+}
+
+func (c Client) Close() error {
+	return c.conn.Close()
+}
+
+func (c Client) RequestPuzzle() ([]byte, []byte, error) {
+	slog.Info("requesting puzzle")
+	c.send(nil)
+
+	resp, err := c.read()
+	if err != nil {
+		return nil, nil, err
+	}
+	slog.Info("got response", "response", resp)
+
+	parts := strings.Split(resp, string(util.Separator))
+	sourceStr, targetStr := parts[0], parts[1]
+	source, err := base64.StdEncoding.DecodeString(sourceStr)
+	if err != nil {
+		slog.Error("could not parse source hash", "error", err)
+		return nil, nil, err
+	}
+	target, err := base64.StdEncoding.DecodeString(targetStr)
+	if err != nil {
+		slog.Error("could not parse target hash", "error", err)
+		return nil, nil, err
+	}
+
+	return source, target, nil
+}
+
+func (c Client) ReqeustQuote(payload string) ([]byte, error) {
+	if err := c.send([]byte(payload)); err != nil {
+		slog.Error("could not send quote request", "error", err, "payload", payload)
+		return nil, err
+	}
+
+	resp, err := c.read()
+	if err != nil {
+		slog.Error("could not read response", "error", err)
+	}
+
+	return []byte(resp), nil
+}
+
+func (c Client) send(body []byte) error {
+	return util.Send(body, c.conn)
+}
+
+func (c Client) read() (string, error) {
+	return util.Read(c.conn)
+}
